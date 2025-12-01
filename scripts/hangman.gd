548baaac7d89db3a997d793ae1ab549015c4ca5b
Rob-Pil8 <121261254+Rob-Pil8@users.noreply.github.com>
2025-12-01 19:09:23 +0100

    Added wrong word error for hangman

--- scripts/hangman.gd
+++ scripts/hangman.gd
@@ -1,9 +1,27 @@
 extends Node2D
 
+@onready var word
 
 func _on_send_button_button_down() -> void:
+	
+	# hide the invalid text label
+	var invalid_text_label = $UI/VBoxContainer/InvalidInputLabel
+	invalid_text_label.hide()
+	
+	# compile the regex to check if the input is valid
 	var regex = RegEx.new()	
 	var input_string: String = $UI/VBoxContainer/HBoxContainer/TextInput.text
 	regex.compile("^[ ]*[A-Za-z]+[ ]*$")
 	
-	print(input_string)
+	# if the input in invalid result is null
+	var result = regex.search(input_string)
+	
+	# show the error if the input isn't valid
+	if not result:
+		invalid_text_label.show()
+		
+	else:
+		word = input_string
+	print(result)
+
+	
