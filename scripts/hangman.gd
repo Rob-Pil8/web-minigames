d9dc83f450f2f58fc4efdf0bad6a3f5a4a0e6e70
Rob-Pil8 <121261254+Rob-Pil8@users.noreply.github.com>
2025-12-03 20:43:49 +0100

    Hangman random word support
    Added support to use a random word for the hangman minigame.
    It chooses one of 360k words.
    Still have to implement how to pass a word to the game, both a random or a user chosen one

--- scripts/hangman.gd
+++ scripts/hangman.gd
@@ -22,6 +22,28 @@ func _on_send_button_button_down() -> void:
 		
 	else:
 		word = input_string
-	print(result)
+	print(word)
 
 	
+
+
+func _on_random_word_button_button_down() -> void:
+	# I have ~365k words to choose from. They are split into 37 files with 10k words each
+	# This chooses a random file, loads it in memory and choose a random word from it
+	var file_number: int = 37
+	var chosen_file: int = (randi() % file_number) +1
+	var file_path = "res://resources/word_list/{number}.txt".format({"number": str(chosen_file)})
+	var words: Array[String] = []
+	var file = FileAccess.open(file_path, FileAccess.READ)
+	
+	if file:
+		while not file.eof_reached():
+			var line = file.get_line().strip_edges()
+			if line != "":
+				words.append(line)
+		file.close()
+		
+	var word_count: int = len(words)
+	var selected_word_index: int = randi() % word_count
+	var selected_word = words[selected_word_index]
+	print(selected_word)
