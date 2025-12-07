64e8810eb525cc8964d3ac21ec75082517608bec
Rob-Pil8 <121261254+Rob-Pil8@users.noreply.github.com>
2025-12-07 12:21:56 +0100

    experimenting mobile browser scaling

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1765105714|203825511';
+const CACHE_VERSION = '1765106438|926956225';
 /** @type {string} */
 const CACHE_PREFIX = 'Web Minigames-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
